2f22ce3c742a79803cd612478cbc5bd567f6f04d
Hasnain Ahmad Khan <hasnain10.official@gmail.com>
2026-03-06 22:27:08 +0500

    Update file.py

--- file.py
+++ file.py
@@ -8,3 +8,12 @@ Github Commands
 5. git restore file-name (to recover deleted file)
 6. git remote add origin (Paste link here)
 7. git remote -v (to check github repo)
+
+
+
+
+This is some thing new about github for me 
+
+i just added this file to this repo and will update this in my local from here
+
+yes.
